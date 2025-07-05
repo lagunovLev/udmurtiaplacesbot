803e0d337cbcace725f1690b1931bec4ac6fe2f8
Lev <lagunovl255@gmail.com>
2025-07-05 22:49:06 +0400

    fix

--- bot/env_variables.py
+++ bot/env_variables.py
@@ -5,9 +5,9 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 
-secret_key = os.environ.get('SECRET_KEY')
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_name = os.environ.get('DB_NAME')
-url = os.environ.get('URL')
-bot_token = os.environ.get('BOT_TOKEN')
+secret_key = os.environ.get('SECRET_KEY') or ""
+db_host = os.environ.get('DB_HOST') or ""
+db_port = os.environ.get('DB_PORT') or ""
+db_name = os.environ.get('DB_NAME') or ""
+url = os.environ.get('URL') or ""
+bot_token = os.environ.get('BOT_TOKEN') or ""
